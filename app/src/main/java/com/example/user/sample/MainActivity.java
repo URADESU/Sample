021c412e3ced2f4a8574bf6546e2b14d713e0661
ab-da <daichi.d.t.524@gmail.com>
2017-09-02 16:25:46 +0900

    Merge branch 'master' of https://github.com/URADESU/Sample
    # Conflicts:
    #	app/src/main/java/com/example/user/sample/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/user/sample/MainActivity.java
+++ app/src/main/java/com/example/user/sample/MainActivity.java
@@ -1,52 +1,21 @@
 package com.example.user.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends FragmentActivity {
+
+    ViewPager viewPager;
 
     @Override
-    public void onClick(View v) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.home_display);
-
-
-        Button bt = (Button) findViewById(R.id.button6);
-        bt.setOnClickListener(new View.OnClickListener() {
-        });
-
-        ImageView iv_droid = (ImageView) findViewById(R.id.iv_droid);
-
-        switch (v.getId()){
-            case R.id.button1:
-                iv_droid.setImageResource(R.drawable.test_image);
-                break;
-
-            case R.id.button2:
-                iv_droid.setImageResource(R.drawable.android_logo_red);
-                break;
-
-            case R.id.button3:
-                iv_droid.setImageResource(R.drawable.android_logo_blue);
-                break;
-        }
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(
+                new Adapter(
+                        getSupportFragmentManager()));
     }
 
-    Button bt_1 = (Button) findViewById(R.id.button1);
-    bt_1.setOnClickListener(this);
-
-    Button bt_2 = (Button) findViewById(R.id.button2);
-    bt_2.setOnClickListener(this);
-
-    Button bt_3 = (Button) findViewById(R.id.button3);
-    bt_3.setOnClickListener(this);
-
-
 }
-
