78f102a4bd3a8dd7e11429c2e96b127ac86d0817
Daichi Takarada <daichi.d.t.524@gmail.com>
2017-09-03 19:16:48 +0900

    リストビューを選択したときにフラグメント3が生成されるようにしました。

--- app/src/main/java/com/example/user/sample/Fragment1.java
+++ app/src/main/java/com/example/user/sample/Fragment1.java
@@ -1,21 +1,22 @@
 package com.example.user.sample;
 
-import com.example.user.sample.R;
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ListView;
+import static com.example.user.sample.R.id.listView;
 
 public class Fragment1 extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -28,26 +29,51 @@ public class Fragment1 extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //listをlistViewに結び付ける
-        ListView list=(ListView)view.findViewById(R.id.listView);
 
         //リストアイテムのラベルを格納する ArrayListをインスタンス化
-        ArrayList<String>labeList=new ArrayList<String>();
+        ArrayList<String>labelList=new ArrayList<>();
 
         //「List Item " "」を20個リストに追加
         for(int i=1;i<=20;i++){
-            labeList.add("List Item"+i);
+            labelList.add("List Item"+i);
         }
+
+        //listをlistViewに結び付ける
+        ListView lv = (ListView)view.findViewById(listView);
 
         //Adapterのインスタンス化
         //第3引数にlabelListを渡す
-        CustomAdapter mAdapter=new CustomAdapter(getActivity(),0,labeList);
+        CustomAdapter mAdapter = new CustomAdapter(getActivity(),0,labelList);
 
         //リストにAdapterをセット
-        list.setAdapter(mAdapter);
+        lv.setAdapter(mAdapter);
+
+
+        //リストが選択された時の処理
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                Fragment3 fg3 = new Fragment3();
+                Bundle bundle = new Bundle();
+                bundle.putInt("selected", position);
+                fg3.setArguments(bundle);
+
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_fragment, fg3);
+
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+            }
+        });
+
 
         //リストアイテムの間の区切り線を非表示にする
-        list.setDivider(null);
+        lv.setDivider(null);
     }
 
 }
